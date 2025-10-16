58287830acb2f90136e2cecc1f638fcfbd757e2e
HTMonkeyG <3254339747@qq.com>
2025-10-17 02:10:06 +0800

    Version 1.7.0 WIP

--- src/includes/htmodloader.h
+++ src/includes/htmodloader.h
@@ -16,8 +16,8 @@
 // Mod loader version.
 // Version number is used for pre-processing statements handling version
 // compatibility.
-#define HTML_VERSION 10600
-#define HTML_VERSION_NAME "1.6.0"
+#define HTML_VERSION 10601
+#define HTML_VERSION_NAME "1.7.0 WIP"
 
 #define HTMLAPI __stdcall
 #ifndef HTMLAPIATTR
